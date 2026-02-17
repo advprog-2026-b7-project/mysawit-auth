623b50e14e938708133292210d4b1872d47815a7
amberleyvs <amberley.vidya41@ui.ac.id>
2026-02-17 08:09:14 +0700

    chore: ci trigger

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     jacoco
     checkstyle
-    id("org.springframework.boot") version "4.0.2"
+    id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "4.4.1.3373"
 
